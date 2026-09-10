16be7d621a0e453358190b323e498dd72bc4aedc
Amr <stall_spring4i@icloud.com>
2026-09-10 12:39:14 +0100

    Add Cask for Tileen version 0.8

---
+++ Casks/tileen.rb
@@ -0,0 +1,11 @@
+cask "tileen" do
+  version "0.8"
+  sha256 "589bb093280e76a3563e7105000d1b7f5206a5cb74a792786ea61ebd189af2ff"
+
+  url "https://github.com/AmrDevoloper/Tileen/releases/download/#{version}/Tileen.app.zip"
+  name "Tileen"
+  desc "Menu bar productivity timer"
+  homepage "https://github.com/AmrDevoloper/Tileen"
+
+  app "Tileen.app"
+end
